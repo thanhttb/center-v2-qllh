907b790084f7788b16ea50f4f6e3fdd7e2a90441
BuiQuangMinh30 <buiquangminh30102001@gmail.com>
2023-04-28 13:00:22 +0700

    add table grid

--- src/pages/dashboard/GeneralDashboardPage.tsx
+++ src/pages/dashboard/GeneralDashboardPage.tsx
@@ -1,8 +1,22 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Container, Grid, Stack, Button } from '@mui/material';
+import { Container, Grid, Stack, Button, Tooltip } from '@mui/material';
+import {
+  DataGridPro,
+  GridRow,
+  GridColumnHeaders,
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridToolbarFilterButton,
+} from '@mui/x-data-grid-pro';
+import { useDemoData } from '@mui/x-data-grid-generator/';
+import { IconButton } from '@mui/material';
+import CreateIcon from '@mui/icons-material/Create';
+import AccessibilityNewRoundedIcon from '@mui/icons-material/AccessibilityNewRounded';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import QueueIcon from '@mui/icons-material/Queue';
 // auth
 import { useAuthContext } from '../../auth/useAuthContext';
 // _mock_
@@ -16,19 +30,6 @@ import {
 // components
 import { useSettingsContext } from '../../components/settings';
 // sections
-// import {
-//   AppWidget,
-//   AppWelcome,
-//   AppFeatured,
-//   AppNewInvoice,
-//   AppTopAuthors,
-//   AppTopRelated,
-//   AppAreaInstalled,
-//   AppWidgetSummary,
-//   AppCurrentDownload,
-//   AppTopInstalledCountries,
-// } from '../../sections/@dashboard/general/app';
-
 import {
   DashboardWidgetSummary,
   DashboardSelectDate,
@@ -47,6 +48,80 @@ export default function GeneralDashboardPage() {
   const [filterStartDate, setFilterStartDate] = useState<Date | null>(null);
 
   const { themeStretch } = useSettingsContext();
+
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 100,
+    maxColumns: 10,
+  });
+
+  const rowsData = React.useMemo(() => {
+    if (!data.rows || data.rows.length === 0) {
+      return { rows: data.rows };
+    }
+    const [firstRow, secondRow, thirdRow, ...rows] = data.rows;
+    return {
+      rows,
+      pinnedRows: {
+        top: [firstRow],
+        bottom: [secondRow, thirdRow],
+      },
+    };
+  }, [data.rows]);
+
+  const handleDelete = (id: any) => {
+    console.log(`Xóa dòng có id ${id}`);
+    // Xử lý xóa tại đây
+  };
+  const columns = [
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      sortable: true,
+      width: 150,
+      renderCell: (params: any) => {
+        return (
+          <>
+            <Tooltip title="Sửa ca học">
+              <IconButton>
+                <CreateIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Điểm danh ca học">
+              <IconButton>
+                <AccessibilityNewRoundedIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Mở khóa khoa học">
+              <IconButton aria-label="Xóa" onClick={() => handleDelete(params.id)}>
+                <LockOpenIcon />
+              </IconButton>
+            </Tooltip>
+          </>
+        );
+      },
+    },
+    { field: 'desk', headerName: 'Desk', width: 100 },
+    { field: 'commodity', headerName: 'Commodity', width: 150 },
+    { field: 'age', headerName: 'Age', type: 'number', width: 100 },
+    { field: 'email', headerName: 'Email', width: 300 },
+  ];
+  function MyToolbar() {
+    return (
+      <GridToolbarContainer sx={{justifyContent: 'flex-end'}}> 
+        {/* <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+        <GridToolbarDensitySelector /> */}
+        <GridToolbarFilterButton />
+        <GridToolbarExport />
+        <Tooltip title="Thêm mới ca học">
+              <IconButton aria-label="Xóa">
+                <QueueIcon />
+              </IconButton>
+            </Tooltip>
+      </GridToolbarContainer>
+    );
+  }
 
   return (
     <>
@@ -70,7 +145,7 @@ export default function GeneralDashboardPage() {
             />
           </Grid>
           <Grid item xs={12} md={8} container spacing={2}>
-            <Grid item xs={4} md={2}>
+            <Grid item xs={4} md={3}>
               <DashboardWidgetSummary
                 title="Total Active Users"
                 percent={2.6}
@@ -81,181 +156,39 @@ export default function GeneralDashboardPage() {
                 }}
               />
             </Grid>
-            <Grid item xs={4} md={2}>
-              <DashboardWidgetSummary
-                title="Total Active Users"
-                percent={2.6}
-                total={18765}
-                chart={{
-                  colors: [theme.palette.primary.main],
-                  series: [5, 18, 12, 51, 68, 11, 39, 37, 27, 20],
-                }}
-              />
-            </Grid>
-            <Grid item xs={4} md={2}>
-              <DashboardWidgetSummary
-                title="Total Active Users"
-                percent={2.6}
-                total={18765}
-                chart={{
-                  colors: [theme.palette.primary.main],
-                  series: [5, 18, 12, 51, 68, 11, 39, 37, 27, 20],
-                }}
-              />
-            </Grid>
-            <Grid item xs={4} md={2}>
-              <DashboardWidgetSummary
-                title="Total Active Users"
-                percent={2.6}
-                total={18765}
-                chart={{
-                  colors: [theme.palette.primary.main],
-                  series: [5, 18, 12, 51, 68, 11, 39, 37, 27, 20],
-                }}
-              />
-            </Grid>
-            <Grid item xs={4} md={2}>
-              <DashboardWidgetSummary
-                title="Total Active Users"
-                percent={2.6}
-                total={18765}
-                chart={{
-                  colors: [theme.palette.primary.main],
-                  series: [5, 18, 12, 51, 68, 11, 39, 37, 27, 20],
-                }}
-              />
-            </Grid>
-            <Grid item xs={4} md={2}>
-              <DashboardWidgetSummary
-                title="Total Active Users"
-                percent={2.6}
-                total={18765}
-                chart={{
-                  colors: [theme.palette.primary.main],
-                  series: [5, 18, 12, 51, 68, 11, 39, 37, 27, 20],
-                }}
-              />
-            </Grid>
-          </Grid>
-          {/* Table  */}
-          {/* <Grid item xs={12} md={4}>
-            <AppWidgetSummary
-              title="Total Installed"
-              percent={0.2}
-              total={4876}
-              chart={{
-                colors: [theme.palette.info.main],
-                series: [20, 41, 63, 33, 28, 35, 50, 46, 11, 26],
-              }}
-            />
           </Grid>
 
-          <Grid item xs={12} md={4}>
-            <AppWidgetSummary
-              title="Total Downloads"
-              percent={-0.1}
-              total={678}
-              chart={{
-                colors: [theme.palette.warning.main],
-                series: [8, 9, 31, 8, 16, 37, 8, 33, 46, 31],
-              }}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentDownload
-              title="Current Download"
-              chart={{
-                colors: [
-                  theme.palette.primary.main,
-                  theme.palette.info.main,
-                  theme.palette.error.main,
-                  theme.palette.warning.main,
-                ],
-                series: [
-                  { label: 'Mac', value: 12244 },
-                  { label: 'Window', value: 53345 },
-                  { label: 'iOS', value: 44313 },
-                  { label: 'Android', value: 78343 },
-                ],
-              }}
-            />
-          </Grid> */}
-          {/* 
-          <Grid item xs={12} md={6} lg={8}>
-            <AppAreaInstalled
-              title="Area Installed"
-              subheader="(+43%) than last year"
-              chart={{
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-                series: [
-                  {
-                    year: '2019',
-                    data: [
-                      { name: 'Asia', data: [10, 41, 35, 51, 49, 62, 69, 91, 148] },
-                      { name: 'America', data: [10, 34, 13, 56, 77, 88, 99, 77, 45] },
-                    ],
+          <Grid
+            item
+            xs={12}
+            md={12}
+            sx={{
+              boxShadow: theme.customShadows.dropdown,
+              borderRadius: 2,
+              m: 2,
+            }}
+          >
+            <div style={{ width: '100%' }}>
+              <DataGridPro
+                {...data}
+                rows={rowsData.rows}
+                columns={columns}
+                rowSelection={false}
+                initialState={{
+                  ...data.initialState,
+                  pagination: {
+                    ...data.initialState?.pagination,
+                    paginationModel: { pageSize: 5 },
                   },
-                  {
-                    year: '2020',
-                    data: [
-                      { name: 'Asia', data: [148, 91, 69, 62, 49, 51, 35, 41, 10] },
-                      { name: 'America', data: [45, 77, 99, 88, 77, 56, 13, 34, 10] },
-                    ],
-                  },
-                ],
-              }}
-            />
-          </Grid>
-
-          <Grid item xs={12} lg={8}>
-            <AppNewInvoice
-              title="New Invoice"
-              tableData={_appInvoices}
-              tableLabels={[
-                { id: 'id', label: 'Invoice ID' },
-                { id: 'category', label: 'Category' },
-                { id: 'price', label: 'Price' },
-                { id: 'status', label: 'Status' },
-                { id: '' },
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTopRelated title="Top Related Applications" list={_appRelated} />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTopInstalledCountries title="Top Installed Countries" list={_appInstalled} />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTopAuthors title="Top Authors" list={_appAuthors} />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <Stack spacing={3}>
-              <AppWidget
-                title="Conversion"
-                total={38566}
-                icon="eva:person-fill"
-                chart={{
-                  series: 48,
                 }}
-              />
-
-              <AppWidget
-                title="Applications"
-                total={55566}
-                icon="eva:email-fill"
-                color="info"
-                chart={{
-                  series: 75,
+                components={{
+                  Toolbar: MyToolbar,
                 }}
+                pagination
+                pageSizeOptions={[5, 10, 25, 50, 100]}
               />
-            </Stack>
-          </Grid> */}
+            </div>
+          </Grid>
         </Grid>
       </Container>
     </>
--- src/sections/@dashboard/general/dashboard/DashboardSelectDate.tsx
+++ src/sections/@dashboard/general/dashboard/DashboardSelectDate.tsx
@@ -1,106 +1,213 @@
 // @mui
-import { Stack, InputAdornment, TextField, MenuItem, Button,Chip, FormControl } from '@mui/material';
+import {
+  Stack,
+  Card,
+  TextField,
+  MenuItem,
+  CardProps,
+  Chip,
+  FormControl,
+  Box,
+  Button,
+} from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
 // components
 import Iconify from '../../../../components/iconify';
-import { alpha } from '@mui/material/styles';
+import { alpha, styled } from '@mui/material/styles';
 
-import FormProvider, {
-    RHFAutocomplete,
-  } from '../../../../components/hook-form';
+import { Theme, useTheme } from '@mui/material/styles';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import React,{ useState } from 'react';
 
-const TAGS_OPTION = [
-    'Toy Story 3',
-    'Logan',
-    'Full Metal Jacket',
-    'Dangal',
-    'The Sting',
-    '2001: A Space Odyssey',
-    "Singin' in the Rain",
-    'Toy Story',
-    'Bicycle Thieves',
-    'The Kid',
-    'Inglourious Basterds',
-    'Snatch',
-    '3 Idiots',
-  ];
-// mui
-// import { useTheme } from '@mui/material/styles';
+
+
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
+
+
+
+const BootstrapButton = styled(Button)({
+  boxShadow: 'none',
+  textTransform: 'none',
+  fontSize: 16,
+  padding: '6px 12px',
+  border: '1px solid',
+  width: '100%',
+  lineHeight: 1.5,
+  backgroundColor: '#8bc34a',
+  borderColor: '#8bc34a',
+  fontWeight: 400,
+  fontFamily: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    '"Segoe UI"',
+    'Roboto',
+    '"Helvetica Neue"',
+    'Arial',
+    'sans-serif',
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(','),
+  '&:hover': {
+    backgroundColor: '#fff',
+    borderColor: '#8bc34a',
+    boxShadow: 'none',
+    color: '#8bc34a'
+  },
+  '&:active': {
+    backgroundColor: '#8bc34a',
+    borderColor: '#8bc34a',
+    boxShadow: 'none',
+    color: '#fffff'
+  },
+  '&:focus': {
+    boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+  },
+});
 // ----------------------------------------------------------------------
 
 const INPUT_WIDTH = 180;
 
-type Props = {
+interface Props extends CardProps {
   filterEndDate: Date | null;
   filterStartDate: Date | null;
   onFilterStartDate: (value: Date | null) => void;
   onFilterEndDate: (value: Date | null) => void;
-};
+}
+
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 export default function DashboardSelectdate({
   filterEndDate,
   filterStartDate,
   onFilterEndDate,
   onFilterStartDate,
-   ...other
+  sx,
+  ...other
 }: Props) {
-    // const theme = useTheme();
+  const theme = useTheme();
+  const [personName, setPersonName] = useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    );
+  };
   return (
-    <Stack
-      spacing={1}
-      direction={{
-        xs: 'column',
-        md: 'row',
+    <Card
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        p: 1,
+        ...sx,
+        boxShadow: theme.customShadows.dropdown,
       }}
-      
-      sx={{ p:1, justifyContent: 'space-between',borderRadius: "16px" }}
       {...other}
     >
-     
-      <DatePicker
-        label="Start date"
-        value={filterStartDate}
-        onChange={onFilterStartDate}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            fullWidth
-            sx={{
-              maxWidth: { md: INPUT_WIDTH },
-            }}
-          />
-        )}
-      />
+      <Stack
+        spacing={1}
+        direction={{
+          xs: 'column',
+          md: 'row',
+        }}
+        {...other}
+      >
+        <DatePicker
+          label="Từ ngày"
+          value={filterStartDate}
+          onChange={onFilterStartDate}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              fullWidth
+              // sx={{
+              //   maxWidth: { md: INPUT_WIDTH },
+              // }}
+            />
+          )}
+        />
 
-      <DatePicker
-        label="End date"
-        value={filterEndDate}
-        onChange={onFilterEndDate}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            fullWidth
-            sx={{
-              maxWidth: { md: INPUT_WIDTH },
-            }}
-          />
+        <DatePicker
+          label="Đến ngày"
+          value={filterEndDate}
+          onChange={onFilterEndDate}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              fullWidth
+              // sx={{
+              //   maxWidth: { md: INPUT_WIDTH },
+              // }}
+            />
+          )}
+        />
+      </Stack>
+
+      <Select
+        labelId="demo-multiple-chip-label"
+        id="demo-multiple-chip"
+        multiple
+        sx={{ width: '100%', p: 0, marginY: 1 }}
+        value={personName}
+        onChange={handleChange}
+        input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+        renderValue={(selected) => (
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+            {selected.map((value) => (
+              <Chip key={value} label={value} />
+            ))}
+          </Box>
         )}
-      />
-      {/* <FormControl>
-       <RHFAutocomplete
-                  name="tags"
-                  multiple
-                  freeSolo
-                //   onChange={(event, newValue) => setValue('tags', newValue)}
-                  options={TAGS_OPTION.map((option) => option)}
-                  renderTags={(value, getTagProps) =>
-                    value.map((option, index) => (
-                      <Chip {...getTagProps({ index })} key={option} size="small" label={option} />
-                    ))
-                  }
-                  renderInput={(params) => <TextField label="Tags" {...params} />}
-                />
-                </FormControl> */}
-    </Stack>
+        MenuProps={MenuProps}
+      >
+        {names.map((name) => (
+          <MenuItem key={name} value={name} style={getStyles(name, personName, theme)}>
+            {name}
+          </MenuItem>
+        ))}
+      </Select>
+
+      <BootstrapButton variant="contained" disableRipple>
+        LÀM MỚI
+      </BootstrapButton>
+
+     
+    </Card>
   );
 }
